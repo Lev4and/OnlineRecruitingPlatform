33a15a969e4712cc7e96bdbdb2647220d191c7eb
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-03-26 14:10:14 +0500

    Выполнено тестирование репозитория EFIdentityRolesRepository.

---
+++ OnlineRecruitingPlatform.Model.Tests/Database/DatabaseBaseTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineRecruitingPlatform.Model.Database;
+using OnlineRecruitingPlatform.Model.Database.Repositories.EntityFramework;
+
+namespace OnlineRecruitingPlatform.Model.Tests.Database
+{
+    public class DatabaseBaseTest
+    {
+        private readonly OnlineRecruitingPlatformDbContext _context;
+        private protected readonly DataManager _dataManager;
+
+        public DatabaseBaseTest()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OnlineRecruitingPlatformDbContext>();
+            var options = optionsBuilder.UseSqlServer(@"Server=DESKTOP-9CDGA5B;Database=OnlineRecruitingPlatform;User ID=sa;Password=sa;Trusted_Connection=True;").Options;
+
+            _context = new OnlineRecruitingPlatformDbContext(options);
+            _dataManager = new DataManager(new EFIdentityRolesRepository(_context));
+        }
+    }
+}
---
+++ OnlineRecruitingPlatform.Model.Tests/Database/Repositories/EntityFramework/EFIdentityRolesRepositoryTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Xunit;
+
+namespace OnlineRecruitingPlatform.Model.Tests.Database.Repositories.EntityFramework
+{
+    public class EFIdentityRolesRepositoryTests : DatabaseBaseTest
+    {
+        [Fact]
+        public async Task GetIdentityRoles_WithoutAnyParams_ReturnNotBeNullCollection()
+        {
+            var result = await Task.Run<List<IdentityRole>>(() =>
+            {
+                return _dataManager.Roles.GetIdentityRoles().ToList();
+            });
+
+            result.Should().BeOfType<List<IdentityRole>>();
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetMainIdentityRoles__WithoutAnyParams_ReturnNotBeNullCollection()
+        {
+            var result = await Task.Run<List<IdentityRole>>(() =>
+            {
+                return _dataManager.Roles.GetMainIdentityRoles().ToList();
+            });
+
+            result.Should().BeOfType<List<IdentityRole>>();
+            result.Should().NotBeNull();
+            result.Should().NotContain(r => r.Name == "Администратор");
+        }
+    }
+}
