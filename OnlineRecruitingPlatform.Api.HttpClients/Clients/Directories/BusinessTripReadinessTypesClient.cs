89dc26695625e6b6eda2138fcac77b5cf8023186
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-04 23:40:27 +0500

    Написан HttpClient для контроллера API BusinessTripReadinessTypesController.

---
+++ OnlineRecruitingPlatform.Api.HttpClients/Clients/Directories/BusinessTripReadinessTypesClient.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.Api.HttpClients.Clients.Directories
+{
+    public class BusinessTripReadinessTypesClient :  BaseHttpClient
+    {
+        public BusinessTripReadinessTypesClient() : base("directories/businessTripReadinessTypes")
+        {
+
+        }
+
+        public BusinessTripReadinessTypesClient(HttpClient client) : base (client, "directories/businessTripReadinessTypes")
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> GetBusinessTripReadinessTypes()
+        {
+            return await _client.GetAsync("");
+        }
+
+        public async Task<HttpResponseMessage> GetBusinessTripReadiness(Guid id)
+        {
+            return await _client.GetAsync($"{id}");
+        }
+
+        public async Task<HttpResponseMessage> AddBusinessTripReadiness(BusinessTripReadiness businessTripReadiness)
+        {
+            return await _client.PostAsync("", new StringContent($"{JsonConvert.SerializeObject(businessTripReadiness)}", Encoding.UTF8, "application/json"));
+        }
+
+        public async Task<HttpResponseMessage> UpdateBusinessTripReadiness(BusinessTripReadiness businessTripReadiness)
+        {
+            return await _client.PutAsync("", new StringContent($"{JsonConvert.SerializeObject(businessTripReadiness)}", Encoding.UTF8, "application/json"));
+        }
+
+        public async Task<HttpResponseMessage> DeleteBusinessTripReadiness(Guid id)
+        {
+            return await _client.DeleteAsync($"{id}");
+        }
+    }
+}
