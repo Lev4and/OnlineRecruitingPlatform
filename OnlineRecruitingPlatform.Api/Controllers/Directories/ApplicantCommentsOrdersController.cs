18878229a3b8a80ecabed9dce6e727a46aeaa849
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-04 16:25:19 +0500

    Написан контроллер API для сущности ApplicantCommentsOrder.

---
+++ OnlineRecruitingPlatform.Api/Controllers/Directories/ApplicantCommentsOrdersController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineRecruitingPlatform.Model.Database;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.Api.Controllers.Directories
+{
+    [ApiController]
+    [Route("api/directories/applicantCommentsOrders/")]
+    [Produces("application/json")]
+    public class ApplicantCommentsOrdersController : Controller
+    {
+        private readonly DataManager _dataManager;
+
+        public ApplicantCommentsOrdersController(DataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetApplicantCommentsOrders()
+        {
+            return Ok(await Task.Run<IQueryable<ApplicantCommentsOrder>>(() =>
+            {
+                return _dataManager.ApplicantCommentsOrders.GetApplicantCommentsOrders();
+            }));
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetApplicantCommentsOrder(Guid id)
+        {
+            try
+            {
+                if(id == null)
+                {
+                    return BadRequest();
+                }
+
+                return Ok(await Task.Run<ApplicantCommentsOrder>(() =>
+                {
+                    return _dataManager.ApplicantCommentsOrders.GetApplicantCommentsOrder(id, true);
+                }));
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddApplicantCommentsOrder([FromBody]ApplicantCommentsOrder applicantCommentsOrder)
+        {
+            try
+            {
+                if(applicantCommentsOrder == null ? true : applicantCommentsOrder.Id != default)
+                {
+                    return BadRequest();
+                }
+
+                return Ok(await Task.Run<bool>(() =>
+                {
+                    return _dataManager.ApplicantCommentsOrders.SaveApplicantCommentsOrder(applicantCommentsOrder);
+                }));
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateApplicantCommentsOrder([FromBody]ApplicantCommentsOrder applicantCommentsOrder)
+        {
+            try
+            {
+                if(applicantCommentsOrder == null ? true : applicantCommentsOrder.Id == default)
+                {
+                    return BadRequest();
+                }
+
+                return Ok(await Task.Run<bool>(() =>
+                {
+                    return _dataManager.ApplicantCommentsOrders.SaveApplicantCommentsOrder(applicantCommentsOrder);
+                }));
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+    }
+}
