f9b71fb979efa99c2f64ba1aed5e9964b1f6c485
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-04 23:18:07 +0500

    Написан HttpClient для контроллера API ApplicantCommentAccessTypesController.

---
+++ OnlineRecruitingPlatform.Api.HttpClients/Clients/Directories/ApplicantCommentAccessTypesClient.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.Api.HttpClients.Clients.Directories
+{
+    public class ApplicantCommentAccessTypesClient : BaseHttpClient
+    {
+        public ApplicantCommentAccessTypesClient() : base("directories/applicantCommentAccessTypes")
+        {
+
+        }
+
+        public ApplicantCommentAccessTypesClient(HttpClient client) : base(client, "directories/applicantCommentAccessTypes")
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> GetApplicantCommentAccessTypes()
+        {
+            return await _client.GetAsync("");
+        }
+
+        public async Task<HttpResponseMessage> GetApplicantCommentAccessType(Guid id)
+        {
+            return await _client.GetAsync($"{id}");
+        }
+
+        public async Task<HttpResponseMessage> AddApplicantCommentAccessType(ApplicantCommentAccessType applicantCommentAccessType)
+        {
+            return await _client.PostAsync($"", new StringContent($"{JsonConvert.SerializeObject(applicantCommentAccessType)}", Encoding.UTF8, "application/json"));
+        }
+
+        public async Task<HttpResponseMessage> UpdateApplicantCommentAccessType(ApplicantCommentAccessType applicantCommentAccessType)
+        {
+            return await _client.PutAsync($"", new StringContent($"{JsonConvert.SerializeObject(applicantCommentAccessType)}", Encoding.UTF8, "application/json"));
+        }
+
+        public async Task<HttpResponseMessage> DeleteApplicantCommentAccessType(Guid id)
+        {
+            return await _client.DeleteAsync($"{id}");
+        }
+    }
+}
---
+++ OnlineRecruitingPlatform.Api.HttpClients/Services/ConfigHttpClients.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OnlineRecruitingPlatform.Api.HttpClients.Services
+{
+    public class ConfigHttpClients
+    {
+        public static string UserAgent { get; set; }
+
+        public static string Protocol { get; set; }
+
+        public static string Domain { get; set; }
+
+        public static string Port { get; set; }
+
+        public static string GetAddressServer()
+        {
+            return $"{Protocol}://{Domain}{(Port.Length > 0 ? $":{Port}" : "")}/api/";
+        }
+
+        public static string GetAddressServer(string controllerName)
+        {
+            if (controllerName != null ? controllerName.Length == 0 : true)
+            {
+                throw new ArgumentNullException("controllerName", "Название контроллера не может быть пустым или длиной 0 символов.");
+            }
+
+            return $"{Protocol}://{Domain}{(Port != null ? $":{Port}" : "")}/api/{controllerName}/";
+        }
+    }
+}
