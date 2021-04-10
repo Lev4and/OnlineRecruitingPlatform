02c3bf91a32e3c98b6bcdb0b2309d6c79f20f32a
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-10 15:33:44 +0500

    Написан и протестирован HttpClient к справочнику API HeadHunter "language_level".

---
+++ OnlineRecruitingPlatform.HttpClients.Tests/HeadHunter/Clients/Directories/LanguageLevelsClientTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories;
+using OnlineRecruitingPlatform.Model.API.HeadHunter.Directories;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineRecruitingPlatform.HttpClients.Tests.HeadHunter.Clients.Directories
+{
+    public class LanguageLevelsClientTests
+    {
+        private readonly LanguageLevelsClient _client;
+
+        public LanguageLevelsClientTests()
+        {
+            _client = new LanguageLevelsClient();
+        }
+
+        [Fact]
+        public async Task GetLanguageLevels_WithoutAnyParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetLanguageLevels();
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<LanguageLevelsDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.LanguageLevels.Should().BeOfType<LanguageLevel[]>();
+            result.LanguageLevels.Should().HaveCountGreaterThan(0);
+        }
+    }
+}
---
+++ OnlineRecruitingPlatform.HttpClients/HeadHunter/Clients/Directories/LanguageLevelsClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories
+{
+    public class LanguageLevelsClient : BaseHttpClient
+    {
+        public LanguageLevelsClient() : base("dictionaries/")
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> GetLanguageLevels()
+        {
+            try
+            {
+                return await _client.GetAsync("");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
