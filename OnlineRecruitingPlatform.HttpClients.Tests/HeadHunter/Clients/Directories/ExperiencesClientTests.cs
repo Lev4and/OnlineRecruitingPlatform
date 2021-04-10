eea9cc5282b7600b0057a974f859eb7861cc0684
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-10 15:23:01 +0500

    Написан и протестирован HttpClient к справочнику API HeadHunter "experience".

---
+++ OnlineRecruitingPlatform.HttpClients.Tests/HeadHunter/Clients/Directories/ExperiencesClientTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories;
+using OnlineRecruitingPlatform.Model.API.HeadHunter.Directories;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineRecruitingPlatform.HttpClients.Tests.HeadHunter.Clients.Directories
+{
+    public class ExperiencesClientTests
+    {
+        private readonly ExperiencesClient _client;
+
+        public ExperiencesClientTests()
+        {
+            _client = new ExperiencesClient();
+        }
+
+        [Fact]
+        public async Task GetExperiences_WithoutAnyParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetExperiences();
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ExperiencesDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.Experiences.Should().BeOfType<Experience[]>();
+            result.Experiences.Should().HaveCountGreaterThan(0);
+        }
+    }
+}
---
+++ OnlineRecruitingPlatform.HttpClients/HeadHunter/Clients/Directories/ExperiencesClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories
+{
+    public class ExperiencesClient : BaseHttpClient
+    {
+        public ExperiencesClient() : base("dictionaries/")
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> GetExperiences()
+        {
+            try
+            {
+                return await _client.GetAsync("");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
