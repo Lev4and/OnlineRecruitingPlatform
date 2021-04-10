ecef067155306c77b02bd9d84b144c8b4515baed
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-10 14:19:00 +0500

    Написан и протестирован HttpClient к справочнику API HeadHunter "education_level".

---
+++ OnlineRecruitingPlatform.HttpClients.Tests/HeadHunter/Clients/Directories/EducationLevelsClientTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories;
+using OnlineRecruitingPlatform.Model.API.HeadHunter.Directories;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineRecruitingPlatform.HttpClients.Tests.HeadHunter.Clients.Directories
+{
+    public class EducationLevelsClientTests
+    {
+        private readonly EducationLevelsClient _client;
+
+        public EducationLevelsClientTests()
+        {
+            _client = new EducationLevelsClient();
+        }
+
+        [Fact]
+        public async Task GetEducationLevels_WithoutAnyParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetEducationLevels();
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<EducationLevelsDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.EducationLevels.Should().BeOfType<EducationLevel[]>();
+            result.EducationLevels.Should().HaveCountGreaterThan(0);
+        }
+    }
+}
