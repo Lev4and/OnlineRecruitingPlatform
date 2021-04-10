6f87a2b588ce107479950685dac5f0eb7bcb85b1
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-10 14:27:43 +0500

    Написан и протестирован HttpClient к справочнику API HeadHunter "employer_active_vacancies_order".

---
+++ OnlineRecruitingPlatform.HttpClients.Tests/HeadHunter/Clients/Directories/EmployerActiveVacanciesOrdersClientTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories;
+using OnlineRecruitingPlatform.Model.API.HeadHunter.Directories;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineRecruitingPlatform.HttpClients.Tests.HeadHunter.Clients.Directories
+{
+    public class EmployerActiveVacanciesOrdersClientTests
+    {
+        private readonly EmployerActiveVacanciesOrdersClient _client;
+
+        public EmployerActiveVacanciesOrdersClientTests()
+        {
+            _client = new EmployerActiveVacanciesOrdersClient();
+        }
+
+        [Fact]
+        public async Task GetEmployerActiveVacanciesOrders_WithoutAnyParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetEmployerActiveVacanciesOrders();
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<EmployerActiveVacanciesOrdersDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.EmployerActiveVacanciesOrders.Should().BeOfType<EmployerActiveVacanciesOrder[]>();
+            result.EmployerActiveVacanciesOrders.Should().HaveCountGreaterThan(0);
+        }
+    }
+}
---
+++ OnlineRecruitingPlatform.HttpClients/HeadHunter/Clients/Directories/EmployerActiveVacanciesOrdersClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories
+{
+    public class EmployerActiveVacanciesOrdersClient : BaseHttpClient
+    {
+        public EmployerActiveVacanciesOrdersClient() : base("dictionaries/")
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> GetEmployerActiveVacanciesOrders()
+        {
+            try
+            {
+                return await _client.GetAsync("");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
