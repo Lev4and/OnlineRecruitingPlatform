8fd439eb48d476cfc49094c73fd83884c4f45371
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-10 14:39:06 +0500

    Написан и протестирован HttpClient к справочнику API HeadHunter "employer_relation".

---
+++ OnlineRecruitingPlatform.HttpClients.Tests/HeadHunter/Clients/Directories/EmployerRelationsClientTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories;
+using OnlineRecruitingPlatform.Model.API.HeadHunter.Directories;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineRecruitingPlatform.HttpClients.Tests.HeadHunter.Clients.Directories
+{
+    public class EmployerRelationsClientTests
+    {
+        private readonly EmployerRelationsClient _client;
+
+        public EmployerRelationsClientTests()
+        {
+            _client = new EmployerRelationsClient();
+        }
+
+        [Fact]
+        public async Task GetEmployerRelations_WithoutAnyParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetEmployerRelations();
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<EmployerRelationsDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.EmployerRelations.Should().BeOfType<EmployerRelation[]>();
+            result.EmployerRelations.Should().HaveCountGreaterThan(0);
+        }
+    }
+}
---
+++ OnlineRecruitingPlatform.HttpClients/HeadHunter/Clients/Directories/EmployerRelationsClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories
+{
+    public class EmployerRelationsClient : BaseHttpClient
+    {
+        public EmployerRelationsClient() : base("dictionaries/")
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> GetEmployerRelations()
+        {
+            try
+            {
+                return await _client.GetAsync("");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
