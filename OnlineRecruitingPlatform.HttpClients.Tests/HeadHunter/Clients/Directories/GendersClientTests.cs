be14837d6911e7c104c33a2df01c91100949cdac
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-10 15:27:39 +0500

    Написан и протестирован HttpClient к справочнику API HeadHunter "gender".

---
+++ OnlineRecruitingPlatform.HttpClients.Tests/HeadHunter/Clients/Directories/GendersClientTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories;
+using OnlineRecruitingPlatform.Model.API.HeadHunter.Directories;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineRecruitingPlatform.HttpClients.Tests.HeadHunter.Clients.Directories
+{
+    public class GendersClientTests
+    {
+        private readonly GendersClient _client;
+
+        public GendersClientTests()
+        {
+            _client = new GendersClient();
+        }
+
+        [Fact]
+        public async Task GetGenders_WithoutAnyParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetGenders();
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<GendersDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.Genders.Should().BeOfType<Gender[]>();
+            result.Genders.Should().HaveCountGreaterThan(0);
+        }
+    }
+}
---
+++ OnlineRecruitingPlatform.HttpClients/HeadHunter/Clients/Directories/GendersClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories
+{
+    public class GendersClient : BaseHttpClient
+    {
+        public GendersClient() : base("dictionaries/")
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> GetGenders()
+        {
+            try
+            {
+                return await _client.GetAsync("");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
