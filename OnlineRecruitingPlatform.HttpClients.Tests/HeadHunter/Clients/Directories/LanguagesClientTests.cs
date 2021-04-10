4fa794ebb9c3ce0c41f0088d0ca28872acbe91dd
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-10 15:39:51 +0500

    Написан и протестирован HttpClient к справочнику API HeadHunter "languages".

---
+++ OnlineRecruitingPlatform.HttpClients.Tests/HeadHunter/Clients/Directories/LanguagesClientTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineRecruitingPlatform.HttpClients.Tests.HeadHunter.Clients.Directories
+{
+    public class LanguagesClientTests
+    {
+        private readonly LanguagesClient _client;
+
+        public LanguagesClientTests()
+        {
+            _client = new LanguagesClient();
+        }
+
+        [Fact]
+        public async Task GetLanguages_WithoutAnyParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetLanguages();
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Language[]>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.Should().BeOfType<Language[]>();
+            result.Should().HaveCountGreaterThan(0);
+        }
+    }
+}
---
+++ OnlineRecruitingPlatform.HttpClients/HeadHunter/Clients/Directories/LanguagesClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories
+{
+    public class LanguagesClient : BaseHttpClient
+    {
+        public LanguagesClient() : base("languages/")
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> GetLanguages()
+        {
+            try
+            {
+                return await _client.GetAsync("");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
