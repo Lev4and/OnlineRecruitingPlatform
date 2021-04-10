b4eba47946485b1a80427eedebaae0d720adb9d9
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-10 14:32:28 +0500

    Написан и протестирован HttpClient к справочнику API HeadHunter "employer_archived_vacancies_order".

---
+++ OnlineRecruitingPlatform.HttpClients.Tests/HeadHunter/Clients/Directories/EmployerArchivedVacanciesOrdersClientTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories;
+using OnlineRecruitingPlatform.Model.API.HeadHunter.Directories;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineRecruitingPlatform.HttpClients.Tests.HeadHunter.Clients.Directories
+{
+    public class EmployerArchivedVacanciesOrdersClientTests
+    {
+        private readonly EmployerArchivedVacanciesOrdersClient _client;
+
+        public EmployerArchivedVacanciesOrdersClientTests()
+        {
+            _client = new EmployerArchivedVacanciesOrdersClient();
+        }
+
+        [Fact]
+        public async Task GetEmployerArchivedVacanciesOrders_WithoutAnyParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetEmployerArchivedVacanciesOrders();
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<EmployerArchivedVacanciesOrdersDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.EmployerArchivedVacanciesOrders.Should().BeOfType<EmployerArchivedVacanciesOrder[]>();
+            result.EmployerArchivedVacanciesOrders.Should().HaveCountGreaterThan(0);
+        }
+    }
+}
---
+++ OnlineRecruitingPlatform.HttpClients/HeadHunter/Clients/Directories/EmployerArchivedVacanciesOrdersClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories
+{
+    public class EmployerArchivedVacanciesOrdersClient : BaseHttpClient
+    {
+        public EmployerArchivedVacanciesOrdersClient() : base("dictionaries/")
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> GetEmployerArchivedVacanciesOrders()
+        {
+            try
+            {
+                return await _client.GetAsync("");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
