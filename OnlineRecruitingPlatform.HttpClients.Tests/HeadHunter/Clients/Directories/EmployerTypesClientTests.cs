7cf74332cdd00b07f5aabdd44688fb19209d31ba
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-10 14:46:41 +0500

    Написан и протестирован HttpClient к справочнику API HeadHunter "employer_type".

---
+++ OnlineRecruitingPlatform.HttpClients.Tests/HeadHunter/Clients/Directories/EmployerTypesClientTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories;
+using OnlineRecruitingPlatform.Model.API.HeadHunter.Directories;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineRecruitingPlatform.HttpClients.Tests.HeadHunter.Clients.Directories
+{
+    public class EmployerTypesClientTests
+    {
+        private readonly EmployerTypesClient _client;
+
+        public EmployerTypesClientTests()
+        {
+            _client = new EmployerTypesClient();
+        }
+
+        [Fact]
+        public async Task GetEmployerTypes_WithoutAnyParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetEmployerTypes();
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<EmployerTypesDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.EmployerTypes.Should().BeOfType<EmployerType[]>();
+            result.EmployerTypes.Should().HaveCountGreaterThan(0);
+        }
+    }
+}
---
+++ OnlineRecruitingPlatform.HttpClients/HeadHunter/Clients/Directories/EmployerTypesClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories
+{
+    public class EmployerTypesClient : BaseHttpClient
+    {
+        public EmployerTypesClient() : base("dictionaries/")
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> GetEmployerTypes()
+        {
+            try
+            {
+                return await _client.GetAsync("");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
