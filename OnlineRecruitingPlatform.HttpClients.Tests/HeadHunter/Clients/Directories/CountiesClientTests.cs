2f6af4ea07ce19486f0c6201f323d2b7279b0f48
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-24 18:07:07 +0500

    Написан и протестирован HttpClient (HeadHunter API) для сущности Country.

---
+++ OnlineRecruitingPlatform.HttpClients.Tests/HeadHunter/Clients/Directories/CountiesClientTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineRecruitingPlatform.HttpClients.Tests.HeadHunter.Clients.Directories
+{
+    public class CountiesClientTests
+    {
+        private readonly CountiesClient _client;
+
+        public CountiesClientTests()
+        {
+            _client = new CountiesClient();
+        }
+
+        [Fact]
+        public async Task GetCounties_WithoutAnyParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetCounties();
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<CountryIV[]>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.Should().BeOfType<CountryIV[]>();
+            result.Should().HaveCountGreaterThan(0);
+        }
+    }
+}
---
+++ OnlineRecruitingPlatform.HttpClients/HeadHunter/Clients/Directories/CountiesClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Directories
+{
+    public class CountiesClient : BaseHttpClient
+    {
+        public CountiesClient() : base ("areas/countries/")
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> GetCounties()
+        {
+            try
+            {
+                return await _client.GetAsync("");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
