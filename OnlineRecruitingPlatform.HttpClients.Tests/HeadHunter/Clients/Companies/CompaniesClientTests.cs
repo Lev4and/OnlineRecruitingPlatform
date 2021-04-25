7fd74f27e3bdc53158d0aa7ec2c763550091df7f
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-25 16:28:44 +0500

    Написан и протестирован HttpClient для HeadHunter API - CompaniesClient, позволяющий получать данные о компаниях.

---
+++ OnlineRecruitingPlatform.HttpClients.Tests/HeadHunter/Clients/Companies/CompaniesClientTests.cs
@@ -0,0 +1,107 @@
+﻿using FluentAssertions;
+using OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Companies;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using OnlineRecruitingPlatform.Model.API.HeadHunter.Directories;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using Xunit;
+
+namespace OnlineRecruitingPlatform.HttpClients.Tests.HeadHunter.Clients.Companies
+{
+    public class CompaniesClientTests
+    {
+        private readonly CompaniesClient _client;
+
+        public CompaniesClientTests()
+        {
+            _client = new CompaniesClient();
+        }
+
+        [Fact]
+        public async Task GetCompanies_WithNegativeParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetCompanies(-1, -1);
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<CompaniesDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.Companies.Should().BeOfType<CompanyIV[]>();
+            result.Companies.Should().HaveCountGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task GetCompanies_WithNegativePageNumberParam_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetCompanies(-1);
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<CompaniesDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.Companies.Should().BeOfType<CompanyIV[]>();
+            result.Companies.Should().HaveCountGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task GetCompanies_WithNegativePerPageParam_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetCompanies(perPage: -1);
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<CompaniesDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.Companies.Should().BeOfType<CompanyIV[]>();
+            result.Companies.Should().HaveCountGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task GetCompanies_WithLargeSomeKindOfParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetCompanies(perPage: 100000);
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<CompaniesDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.Companies.Should().BeOfType<CompanyIV[]>();
+            result.Companies.Should().HaveCountGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task GetCompanies_WithoutParams_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetCompanies();
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<CompaniesDirectory>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.Companies.Should().BeOfType<CompanyIV[]>();
+            result.Companies.Should().HaveCountGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task GetCompany_WithInvalidParam_ReturnHttpStatusCode404Response()
+        {
+            var response = await _client.GetCompany(-1);
+
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task GetCompany_WithParam_ReturnHttpStatusCode200Response()
+        {
+            var response = await _client.GetCompany(1179134);
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<CompanyIV>(resultJson);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.Should().BeOfType<CompanyIV>();
+            result.Should().NotBeNull();
+        }
+    }
+}
---
+++ OnlineRecruitingPlatform.HttpClients/HeadHunter/Clients/Companies/CompaniesClient.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnlineRecruitingPlatform.HttpClients.HeadHunter.Clients.Companies
+{
+    public class CompaniesClient : BaseHttpClient
+    {
+        public CompaniesClient() : base("employers/")
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> GetCompany(int companyId)
+        {
+            try
+            {
+                return await _client.GetAsync($"{companyId}/");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<HttpResponseMessage> GetCompanies(int pageNumber = 0, int perPage = 100)
+        {
+            try
+            {
+                if (pageNumber < 0)
+                {
+                    pageNumber = 0;
+                }
+
+                if(perPage <= 0 || perPage > 100)
+                {
+                    perPage = 20;
+                }
+
+                return await _client.GetAsync($"?page={pageNumber}&per_page={perPage}");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
