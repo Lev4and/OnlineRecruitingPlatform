0abbe2475afb14f9001b431b4f3fe589c96ae543
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-25 13:31:38 +0500

    Написан репозиторий для сущности CompanyLocation.

---
+++ OnlineRecruitingPlatform.Model/Database/Repositories/Abstract/ICompanyLocationsRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnlineRecruitingPlatform.Model.Database.Entities;
+using System;
+using System.Linq;
+
+namespace OnlineRecruitingPlatform.Model.Database.Repositories.Abstract
+{
+    public interface ICompanyLocationsRepository
+    {
+        bool ContainsCompanyLocation(Guid companyId, Guid areaId);
+
+        bool SaveCompanyLocation(CompanyLocation entity);
+
+        CompanyLocation GetCompanyLocation(Guid id, bool track = false);
+
+        IQueryable<CompanyLocation> GetCompanyLocations(bool track = false);
+
+        void DeleteCompanyLocation(Guid id);
+    }
+}
---
+++ OnlineRecruitingPlatform.Model/Database/Repositories/EntityFramework/EFCompanyLocationsRepository.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using OnlineRecruitingPlatform.Model.Database.Repositories.Abstract;
+using System;
+using System.Linq;
+
+namespace OnlineRecruitingPlatform.Model.Database.Repositories.EntityFramework
+{
+    public class EFCompanyLocationsRepository : ICompanyLocationsRepository
+    {
+        private readonly OnlineRecruitingPlatformDbContext _context;
+
+        public EFCompanyLocationsRepository(OnlineRecruitingPlatformDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool ContainsCompanyLocation(Guid companyId, Guid areaId)
+        {
+            if (companyId == null)
+            {
+                throw new ArgumentNullException("companyId", "Параметр не может быть пустым.");
+            }
+
+            if (areaId == null)
+            {
+                throw new ArgumentNullException("areaId", "Параметр не может быть пустым.");
+            }
+
+            return _context.CompanyLocations.SingleOrDefault(c => c.CompanyId == companyId && c.AreaId == areaId) != null;
+        }
+
+        public bool SaveCompanyLocation(CompanyLocation entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity", "Параметр не может быть пустым.");
+            }
+
+            if (!ContainsCompanyLocation(entity.CompanyId, entity.AreaId))
+            {
+                if (entity.Id == default)
+                {
+                    _context.Entry(entity).State = EntityState.Added;
+                }
+                else
+                {
+                    _context.Entry(entity).State = EntityState.Modified;
+                }
+
+                _context.SaveChanges();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public CompanyLocation GetCompanyLocation(Guid id, bool track = false)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException("id", "Параметр не может быть пустым.");
+            }
+
+            if (track)
+            {
+                return _context.CompanyLocations.SingleOrDefault(c => c.Id == id);
+            }
+            else
+            {
+                return _context.CompanyLocations.AsNoTracking().SingleOrDefault(c => c.Id == id);
+            }
+        }
+
+        public IQueryable<CompanyLocation> GetCompanyLocations(bool track = false)
+        {
+            if (track)
+            {
+                return _context.CompanyLocations;
+            }
+            else
+            {
+                return _context.CompanyLocations.AsNoTracking();
+            }
+        }
+
+        public void DeleteCompanyLocation(Guid id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException("id", "Параметр не может быть пустым.");
+            }
+
+            var entity = GetCompanyLocation(id);
+
+            _context.CompanyLocations.Remove(entity);
+            _context.SaveChanges();
+        }
+    }
+}
