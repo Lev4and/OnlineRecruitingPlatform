bacc6492b70340da60285364a79ba5d7441b0217
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-04-25 13:45:31 +0500

    Написан репозиторий для сущности CompanyLogo.

---
+++ OnlineRecruitingPlatform.Model/Database/Repositories/Abstract/ICompanyLogosRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnlineRecruitingPlatform.Model.Database.Entities;
+using System;
+using System.Linq;
+
+namespace OnlineRecruitingPlatform.Model.Database.Repositories.Abstract
+{
+    public interface ICompanyLogosRepository
+    {
+        bool ContainsCompanyLogo(Guid companyId);
+
+        bool SaveCompanyLogo(CompanyLogo entity);
+
+        CompanyLogo GetCompanyLogo(Guid id, bool track = false);
+
+        IQueryable<CompanyLogo> GetCompanyLogos(bool track = false);
+
+        void DeleteCompanyLogo(Guid id);
+    }
+}
---
+++ OnlineRecruitingPlatform.Model/Database/Repositories/EntityFramework/EFCompanyLogosRepository.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineRecruitingPlatform.Model.Database.Entities;
+using OnlineRecruitingPlatform.Model.Database.Repositories.Abstract;
+using System;
+using System.Linq;
+
+namespace OnlineRecruitingPlatform.Model.Database.Repositories.EntityFramework
+{
+    public class EFCompanyLogosRepository : ICompanyLogosRepository
+    {
+        private readonly OnlineRecruitingPlatformDbContext _context;
+
+        public EFCompanyLogosRepository(OnlineRecruitingPlatformDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool ContainsCompanyLogo(Guid companyId)
+        {
+            if (companyId == null)
+            {
+                throw new ArgumentNullException("companyId", "Параметр не может быть пустым.");
+            }
+
+            return _context.CompanyLogos.SingleOrDefault(c => c.CompanyId == companyId) != null;
+        }
+
+        public bool SaveCompanyLogo(CompanyLogo entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity", "Параметр не может быть пустым.");
+            }
+
+            if (entity.Id == default)
+            {
+                if (!ContainsCompanyLogo(entity.CompanyId))
+                {
+                    _context.Entry(entity).State = EntityState.Added;
+                    _context.SaveChanges();
+
+                    return true;
+                }
+            }
+            else
+            {
+                var oldVersionEntity = GetCompanyLogo(entity.Id);
+
+                if (oldVersionEntity.CompanyId != entity.CompanyId)
+                {
+                    if (!ContainsCompanyLogo(entity.CompanyId))
+                    {
+                        _context.Entry(entity).State = EntityState.Modified;
+                        _context.SaveChanges();
+
+                        return true;
+                    }
+                }
+                else
+                {
+                    _context.Entry(entity).State = EntityState.Modified;
+                    _context.SaveChanges();
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public CompanyLogo GetCompanyLogo(Guid id, bool track = false)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException("id", "Параметр не может быть пустым.");
+            }
+
+            if (track)
+            {
+                return _context.CompanyLogos.SingleOrDefault(c => c.Id == id);
+            }
+            else
+            {
+                return _context.CompanyLogos.AsNoTracking().SingleOrDefault(c => c.Id == id);
+            }
+        }
+
+        public IQueryable<CompanyLogo> GetCompanyLogos(bool track = false)
+        {
+            if (track)
+            {
+                return _context.CompanyLogos;
+            }
+            else
+            {
+                return _context.CompanyLogos.AsNoTracking();
+            }
+        }
+
+        public void DeleteCompanyLogo(Guid id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException("id", "Параметр не может быть пустым.");
+            }
+
+            var entity = GetCompanyLogo(id);
+
+            _context.CompanyLogos.Remove(entity);
+            _context.SaveChanges();
+        }
+    }
+}
